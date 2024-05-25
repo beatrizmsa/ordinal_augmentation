fa99daaf705ea0bf9cfdbdb691c4d1f8f1cf46d8
Beatriz Sa <beatriz.m.sa@inesctec.pt>
2024-05-25 18:28:07 +0000

    add a new dataset

--- torch/data.py
+++ torch/data.py
@@ -1,5 +1,6 @@
 import torch, torchvision
 from skimage.io import imread
+import pandas as pd
 import os
 
 class Smear:
@@ -28,6 +29,39 @@ class Smear:
 
 if __name__ == '__main__':
     ds = Smear('/nas-ctm01/homes/bmsa/data')
+    x, y = ds[0]
+    print('x:', type(x), x.shape, x.dtype, x.min(), x.max())
+    print('y:', y)
+
+
+class Adience:
+    # http://www.cslab.openu.ac.il/download/adiencedb/AdienceBenchmarkOfUnfilteredFacesForGenderAndAgeClassification/ 
+    # user = adiencedb
+    # passeword = adience
+    num_classes = 8
+    classes = ['0-2', '4-6', '8-13', '15-20',
+               '25-32', '38-43', '48-53', '60-']
+    
+    def __init__(self, root, transform=None):
+        self.root = os.path.join(root, 'Adience')
+        df = pd.concat([pd.read_csv(f'fold_{fold}.csv') for fold in range(5)]).drop_duplicates('image_filename')
+        self.files = [(klass, fname) for klass in self.classes for fname in os.listdir(self.root) if fname.endswith('.txt')]
+        self.labels = [self.classes.index(klass) for klass, _ in self.files]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, i):
+        dname, fname = self.files[i]
+        label = self.labels[i]
+        image = imread(os.path.join(self.root, dname, fname))
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+if __name__ == '__main__':
+    ds = Adience('/nas-ctm01/homes/bmsa/data')
     x, y = ds[0]
     print('x:', type(x), x.shape, x.dtype, x.min(), x.max())
     print('y:', y)
