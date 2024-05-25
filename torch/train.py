f47c67a224266d2d9cfff9c6258a448fa3c7c1c1
Beatriz Sa <beatriz.m.sa@inesctec.pt>
2024-05-25 18:27:23 +0000

    change code to use the same device and split the dataset into training and testing

--- torch/train.py
+++ torch/train.py
@@ -1,8 +1,11 @@
 import argparse
 import math
 parser = argparse.ArgumentParser()
+parser.add_argument('dataset')
 parser.add_argument('augmentation')
 parser.add_argument('--tau', type=float, default=0.15)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batchsize', type=int, default=32)
 parser.add_argument('--debug', action='store_true')
 args = parser.parse_args()
 
@@ -18,22 +21,27 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 ######################################### DATA #########################################
 
+print('Loading data...')
+ds = getattr(data, args.dataset)
+
 transforms = v2.Compose([
     v2.ToImage(),
     v2.Resize((int(224*1.1), int(224*1.1)), antialias=True),
     v2.RandomCrop((224, 224)),
     v2.RandomHorizontalFlip(),
-    v2.RandomVerticalFlip(),
+    v2.RandomVerticalFlip(0.5 if ds.allow_vflips else 0),
     v2.ColorJitter(0.1, 0.1),
     v2.ToDtype(torch.float32, scale=True),
 ])
 
-tr = ds = data.Smear(r"C:\Users\Beatriz\Desktop\3 year (2023-2024)\2ºsemestre\estagio\data", transforms)
+ds = ds('/nas-ctm01/homes/bmsa/data', transforms)
+tr, _ = torch.utils.data.random_split(ds, [0.8, 0.2], torch.Generator().manual_seed(42))
 
 # since we want to do augmentation across all the labels, we want to iterate across
 # images from each class at the same time
-ds_per_class = [torch.utils.data.Subset(tr, [i for i, l in enumerate(ds.labels) if l == k]) for k in range(ds.num_classes)]
-ds_per_class = [torch.utils.data.DataLoader(d, 32, True, pin_memory=True) for d in ds_per_class]  # , num_workers=4
+tr_labels = [tr[i][1] for i in range(len(tr))]
+tr_per_class = [torch.utils.data.Subset(tr, [i for i, l in enumerate(tr_labels) if l == k]) for k in range(ds.num_classes)]
+tr_per_class = [torch.utils.data.DataLoader(d, args.batchsize, True, pin_memory=True) for d in tr_per_class]  # , num_workers=4
 
 ######################################### MODEL #########################################
 
@@ -43,8 +51,8 @@ model.to(device)
 
 ###################################### ORDINAL AUG ######################################
 
-def exp(num_classes, center_class, tau):
-    x = torch.arange(num_classes, dtype=torch.float)
+def exp(num_classes, center_class, tau, device):
+    x = torch.arange(num_classes, dtype=torch.float, device= device)
     return torch.nn.functional.softmax(-torch.abs(center_class[:, None] - x[None, :]) / tau, 1)
 
 def none(images):  # images.shape = [N, K]. return.shape = [N]
@@ -58,13 +66,13 @@ def mixup(images):
     device = images.device
     n = images.shape[0]
     num_classes = images.shape[1]
-    labels = torch.zeros((n, num_classes))
+    labels = torch.zeros((n, num_classes), device=device)
     class_1 = torch.randint(0, num_classes, [n], device=device)
     class_2 = torch.randint(0, num_classes-1, [n], device=device)
     class_2[class_2 >= class_1] = 1+class_2[class_2 >= class_1]
 
     beta = torch.distributions.beta.Beta(torch.tensor(alpha), torch.tensor(alpha))
-    temp = beta.sample([n])
+    temp = beta.sample([n]).to(device)
     labels[range(n), class_1] = temp
     labels[range(n), class_2] = 1-temp
 
@@ -75,12 +83,12 @@ def ordinal_adjacent_mixup(images):
     device = images.device
     n = images.shape[0]
     num_classes = images.shape[1]
-    labels = torch.zeros((n, num_classes))
+    labels = torch.zeros((n, num_classes), device=device)
     class_1 = torch.randint(0, num_classes-1, [n], device=device)
     class_2 = 1+class_1  # <-- difference between this and the previous one
 
     beta = torch.distributions.beta.Beta(torch.tensor(alpha), torch.tensor(alpha))
-    temp = beta.sample([n])
+    temp = beta.sample([n]).to(device)
     labels[range(n), class_1] = temp
     labels[range(n), class_2] = 1-temp
 
@@ -93,7 +101,7 @@ def ordinal_exponential_mixup(images):
     num_classes = images.shape[1]
 
     labels = torch.randint(0, num_classes, [n], device=device) + 1
-    labels = exp(num_classes, labels, args.tau)
+    labels = exp(num_classes, labels, args.tau, device)
 
     mixup_images = torch.sum(images * labels[:, :, None, None, None], 1)
     return mixup_images, labels
@@ -121,10 +129,11 @@ def vectorize(fn):
     return f
 
 def normalize_each_nested(images):
+    device = images.device
     output = images[0].clone()
     num_classes = images.shape[0]
     center_class = torch.randint(0, num_classes, [1], device=device)
-    probabilities = exp(num_classes, center_class, args.tau)[0]
+    probabilities = exp(num_classes, center_class, args.tau, device)[0]
     
     each_nested(images, probabilities, 0 , 0 , images[0].shape[2], images[0].shape[1], output)
     return output, probabilities
@@ -169,9 +178,10 @@ def jaime_ordinal_cutmix(images, probabilities, center):
     return output
 
 def normalize_jaime_ordinal_cutmix(images):
+    device = images.device
     num_classes = images.shape[0]
     center_class = torch.randint(0, num_classes, [1], device=device)
-    probabilities = exp(num_classes, center_class, args.tau)[0]
+    probabilities = exp(num_classes, center_class, args.tau,device)[0]
     output = jaime_ordinal_cutmix(images, probabilities, center_class[0])
     return output, probabilities
 
@@ -183,19 +193,22 @@ jaime = vectorize(normalize_jaime_ordinal_cutmix)
 
 ######################################### LOOP #########################################
 
+print('Training...')
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters())
 aug = globals()[args.augmentation]
 
-nits = 10000
+nits = int(round(len(tr) * args.epochs / args.batchsize))
+nprint = len(tr) // args.batchsize
 avg_loss = avg_acc = 0
 tic = time()
 for it in range(nits):
-    images = torch.stack([next(iter(d))[0] for d in ds_per_class], 1)
+    images = torch.stack([next(iter(d))[0] for d in tr_per_class], 1)
     images = images.to(device)
     old_images = images
     # images: input (32, 7, 3, 224, 224) => output (32, 3, 224, 224)
     images, labels = aug(images)
+    #print('images:', images.min(), images.max(), 'labels:', labels)
 
     if args.debug:
         plt.subplot(2, 7, 1)
@@ -213,12 +226,25 @@ for it in range(nits):
     avg_loss += float(loss) / len(tr)
     if len(labels.shape) == 2:
         labels = labels.argmax(1)
-    avg_acc += float((labels == preds.argmax(1)).float().mean()) / len(tr)
+    avg_acc += float((labels == preds.argmax(1)).float().mean()) / nprint
 
-    if (it+1) % 100 == 0:
+    if (it+1) % nprint == 0:
         toc = time()
         print(f'Iteration {it+1}/{nits} - {toc-tic:.0f}s - Avg loss: {avg_loss} - Avg acc: {avg_acc}')
         avg_loss = avg_acc = 0
         tic = time()
 
-torch.save(model, f'model-{args.augmentation}.pth')
+torch.save(model, f'model-{args.dataset}-{args.augmentation}.pth')
+
+'''
+import torchmetrics
+
+acc = torchmetrics.Accuracy(...)
+
+for images, labels in ts:
+    with torch.no_grad():
+        preds = model(images)
+    acc.update(preds.argmax(1), labels)
+print(acc.compute())
+'''
+
--- torch/train.sh
+++ torch/train.sh
@@ -4,12 +4,19 @@
 # srun --partition=gpu_min8GB --qos=gpu_min8GB_ext python3 train.py none
 #
 # gpu_min8GB, gpu_min11GB, gpu_min12GB, gpu_min24GB, gpu_min32GB, gpu_min80GB
-#SBATCH --partition=gpu_min8GB       # Reserved partition
-#SBATCH --qos=gpu_min8GB_ext             # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --partition=gpu_min8gb       # Reserved partition
+#SBATCH --qos=gpu_min8gb_ext             # QoS level. Must match the partition name. External users must add the suffix "_ext".
 #SBATCH --job-name=train             # Job name
 #slurm.%j.out
-#SBATCH -o slurm.txt           # File containing STDOUT output
-#SBATCH -e slurm.txt           # File containing STDERR output
+#SBATCH -o slurm-%j.txt           # File containing STDOUT output
+#SBATCH -e slurm-%j.txt           # File containing STDERR output
 
-echo "train with none augmentation"
-python train.py none
+# about half an hour per method
+
+DATASET="Smear"
+METHODS="none mixup ordinal_adjacent_mixup ordinal_exponential_mixup nested jaime"
+for METHOD in $METHODS; do
+    echo "python train.py $DATASET $METHOD"
+    python -u train.py $DATASET $METHOD
+done
+# rm slurm.txt; sbatch ./train.sh
